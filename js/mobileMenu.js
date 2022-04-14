30690bf027f04c6b9e2fc9478040f71228e612aa
williamrolando88 <william.rolando88@hotmail.com>
2022-04-13 21:42:55 -0500

    Add js to control menu open and close action

---
+++ js/mobileMenu.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body');
+const openMenu = document.querySelector('.fa-bars');
+const mobileMenu = document.querySelector('#mobile-nav');
+const closeMenu = document.querySelectorAll('.close-mobile-nav');
+
+const openNavMenu = () => {
+  body.classList.add('overflow-hidden');
+  mobileMenu.classList.add('flex');
+  mobileMenu.classList.remove('hidden');
+};
+
+const closeNavMenu = () => {
+  body.classList.remove('overflow-hidden');
+  mobileMenu.classList.remove('flex');
+  mobileMenu.classList.add('hidden');
+};
+
+openMenu.addEventListener('click', openNavMenu);
+
+closeMenu.addEventListener('click', closeNavMenu);
